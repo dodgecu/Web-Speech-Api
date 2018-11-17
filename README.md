855f7dd260b211c8f57d58f5afa695abc4eed5be
Pavlo Pihula <rogertaylor1949@gmail.com>
2018-11-17 11:36:35 -0500

    App mock-up

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Web-Speech-Api
+
 A small app based on "Text To Speech" api https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API
+
+# APP demo
+
+http://biz211.inmotionhosting.com/~fading8/voice/index.html
--- src/app.js
+++ src/app.js
@@ -8,7 +8,8 @@ const textForm = document.querySelector("form"),
   rate = document.querySelector("#rate"),
   rateValue = document.querySelector("#rate-value"),
   pitch = document.querySelector("#pitch"),
-  pitchValue = document.querySelector("#pitch-value");
+  pitchValue = document.querySelector("#pitch-value"),
+  body = document.querySelector("body");
 
 // Init voices array
 let voices = [];
@@ -29,3 +30,63 @@ getVoices();
 if (synth.onvoiceschanged !== undefined) {
   synth.onvoiceschanged = getVoices;
 }
+
+// Speak
+const speak = () => {
+  if (synth.speaking) {
+    console.error("Speaking...");
+    return;
+  }
+  if (textInput.value !== "") {
+    // Get Speech from text area
+    body.style.background = "#141414 url(/img/wave.gif)";
+    body.style.backgroundSize = "contain";
+    body.style.backgroundRepeat = "no-repeat";
+    const speakText = new SpeechSynthesisUtterance(textInput.value);
+
+    // Event once speech has been completed
+    speakText.onend = e => {
+      body.style.background = "#141414";
+      console.log("Done...");
+    };
+
+    // Event in case of error
+    speakText.onerror = e => {
+      console.error("Something went wrong...");
+    };
+
+    // Voice
+    const selectedVoice = voiceSelect.selectedOptions[0].getAttribute(
+      "data-name"
+    );
+    // Get selected voice speaking
+    voices.forEach(voice => {
+      if (voice.name === selectedVoice) {
+        speakText.voice = voice;
+      }
+    });
+
+    // Pitch and Rate
+
+    speakText.rate = rate.value;
+    speakText.pitch = pitch.value;
+
+    synth.speak(speakText);
+  }
+};
+
+// Event Listeners
+textForm.addEventListener("submit", e => {
+  e.preventDefault();
+  speak();
+  textInput.blur();
+});
+
+// Rate change
+rate.addEventListener("change", e => (rateValue.textContent = rate.value));
+
+// Pitch change
+pitch.addEventListener("change", e => (pitchValue.textContent = pitch.value));
+
+// Voice Select change
+voiceSelect.addEventListener("change", e => speak());
